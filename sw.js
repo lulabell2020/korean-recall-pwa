b4d735002532b8aed6195c375a393052240ebbc5
lulabell2020 <73671476+lulabell2020@users.noreply.github.com>
2026-04-07 11:05:14 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "korean-recall-v14";
+const CACHE = "korean-recall-v15";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./sw.js"];
 
 self.addEventListener("install", (e) => {
